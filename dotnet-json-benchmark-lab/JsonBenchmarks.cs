1f316b1c25f5ecc8853270be7947233f2dec2024
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2021-05-17 19:25:12 +0700

    Add project files.

---
+++ dotnet-json-benchmark-lab/JsonBenchmarks.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using RestSharp;
+
+namespace dotnet_json_benchmark_lab
+{
+    [MemoryDiagnoser]
+    public class JsonBenchmarks
+    {
+        MarketTrades mNewtonsoftJson;
+        string sNewtonsoftJson;
+        MarketTrades mSystemTextJson;
+        string sSystemTextJson;
+
+        public string GetJsonMarketTrades()
+        {
+            var client = new RestClient("https://api.bitkub.com/api/market/trades?sym=THB_BTC&lmt=100000");
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+        public class MarketTrades
+        {
+            public int Error { get; set; }
+            public object[] Result { get; set; }
+        }
+
+        [Benchmark]
+        public void NewtonsoftJsonDeserializeObject()
+        {
+            mNewtonsoftJson = Newtonsoft.Json.JsonConvert.DeserializeObject<MarketTrades>(GetJsonMarketTrades());
+        }
+
+        [Benchmark]
+        public void SystemTextJsonDeserializeObject()
+        {
+            mSystemTextJson = System.Text.Json.JsonSerializer.Deserialize<MarketTrades>(GetJsonMarketTrades());
+        }
+
+        [Benchmark]
+        public void NewtonsoftJsonSerializeObject()
+        {
+            sNewtonsoftJson = Newtonsoft.Json.JsonConvert.SerializeObject(mNewtonsoftJson);
+        }
+
+        [Benchmark]
+        public void SystemTextJsonSerializeObject()
+        {
+            sSystemTextJson = System.Text.Json.JsonSerializer.Serialize(mSystemTextJson);
+        }
+    }
+}
---
+++ dotnet-json-benchmark-lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace dotnet_json_benchmark_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<JsonBenchmarks>();
+        }
+    }
+}
